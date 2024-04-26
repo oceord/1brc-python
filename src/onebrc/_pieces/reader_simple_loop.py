bda4ec69b1f66cba4cb6b0023ea4d4ab6935deb9
oceord <73301852+oceord@users.noreply.github.com>
2024-04-26 13:29:57 +0100

    Refactor pieces

--- src/onebrc/_pieces/reader_simple_loop.py
+++ src/onebrc/_pieces/reader_simple_loop.py
@@ -1,10 +1,10 @@
 import sys
 from pathlib import Path
 
-from onebrc.measure.timeit import timeit
+from onebrc.measure.timeit import timeit_avg
 
 
-@timeit
+@timeit_avg
 def main(path):
     with path.open() as file:
         for _ in file:
