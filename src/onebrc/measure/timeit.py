7eeea1bf8b4ee356c0958f663b3116b83799f37d
oceord <73301852+oceord@users.noreply.github.com>
2024-04-26 13:30:58 +0100

    Add timeit_avg

--- src/onebrc/measure/timeit.py
+++ src/onebrc/measure/timeit.py
@@ -1,5 +1,6 @@
 import inspect
 import time
+from statistics import mean
 
 
 def timeit(func):
@@ -8,8 +9,29 @@ def timeit(func):
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
-        execution_time = end_time - start_time
-        print(f"\n\t{func_module}.{func.__name__}\t=>\t{execution_time} seconds\n")
+        execution_time = end_time - start_time # seconds
+        print(f"\n\tModule:\t\t{func_module}.{func.__name__}")
+        print(f"\tExecTime:\t{execution_time}\n")
+        return result
+
+    return wrapper
+
+
+def timeit_avg(func):
+    def wrapper(*args, **kwargs):
+        number_of_execs = 10
+        exec_times = []
+        func_module = ".".join(inspect.getmodule(func).__file__.split("/")[-2:])[:-3]
+        for _ in range(number_of_execs):
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            execution_time = end_time - start_time # seconds
+            exec_times.append(execution_time)
+        avg_execution_time = mean(exec_times)
+        print(f"\n\tModule:\t\t{func_module}.{func.__name__}")
+        print(f"\tAvgExecTime:\t{avg_execution_time}")
+        print(f"\tExecTimes:\t{"\n\t\t\t".join(str(t) for t in sorted(exec_times))}\n")
         return result
 
     return wrapper
