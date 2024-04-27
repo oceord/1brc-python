f1625b0216c4e7c2c547add0531010cb2a8fcac6
oceord <73301852+oceord@users.noreply.github.com>
2024-04-27 16:20:31 +0100

    Fix timeit output

--- src/onebrc/measure/timeit.py
+++ src/onebrc/measure/timeit.py
@@ -10,8 +10,8 @@ def timeit(func):
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         execution_time = end_time - start_time # seconds
-        print(f"\nModule:\t\t{func_module}.{func.__name__}")
-        print(f"ExecTime:\t{execution_time}\n")
+        print(f"Module:\n    {func_module}.{func.__name__}")
+        print(f"ExecTime:\n    {execution_time}\n")
         return result
 
     return wrapper
@@ -29,9 +29,9 @@ def timeit_avg(func):
             execution_time = end_time - start_time # seconds
             exec_times.append(execution_time)
         avg_execution_time = mean(exec_times)
-        print(f"\nModule:\t\t{func_module}.{func.__name__}")
-        print(f"AvgExecTime:\t{avg_execution_time}")
-        print(f"ExecTimes:\t{"\n\t\t".join(str(t) for t in sorted(exec_times))}\n")
+        print(f"Module:\n    {func_module}.{func.__name__}")
+        print(f"AvgExecTime:\n    {avg_execution_time}")
+        print(f"ExecTimes:\n    {"\n    ".join(str(t) for t in sorted(exec_times))}\n")
         return result
 
     return wrapper
