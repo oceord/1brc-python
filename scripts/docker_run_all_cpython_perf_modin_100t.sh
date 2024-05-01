2a3cd5b8f72c47723654018720133c96aabe0986
oceord <73301852+oceord@users.noreply.github.com>
2024-05-01 10:53:47 +0100

    Use shm of 8gb instead of 16gb

--- scripts/docker_run_all_cpython_perf_modin_100t.sh
+++ scripts/docker_run_all_cpython_perf_modin_100t.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --privileged --rm -it --shm-size=16gb \
+docker run --privileged --rm -it --shm-size=8gb \
     -v /home/oceord/Dev/1brc-python/test_data:/onebrc/test_data \
     --name onebrc-cpython_perf-modin onebrc-cpython_perf-modin \
     make run-modin-ray-100t-docker
