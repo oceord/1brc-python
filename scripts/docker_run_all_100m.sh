5c49f2b46626e32983e727fb8c3e7be785387182
oceord <73301852+oceord@users.noreply.github.com>
2024-05-04 20:28:06 +0100

    Fix docker_run_all scripts

--- scripts/docker_run_all_100m.sh
+++ scripts/docker_run_all_100m.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 for SCRIPT in "$SCRIPT_DIR"/100m/*.sh; do
-    if [ -f "$SCRIPT" ] && [ -x "$SCRIPT" ]; then
+    if [ -f "$SCRIPT" ]; then
         bash "$SCRIPT"
         sleep 60
     fi
