36e98b285061591244d64ba326249b94f2560d6e
oceord <73301852+oceord@users.noreply.github.com>
2024-05-04 13:27:55 +0100

    Move bash scripts location

--- scripts/docker_run_all_cpython_perf_modin_100t.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-
-docker run --privileged --rm -it --shm-size=8gb \
-    -v /home/oceord/Dev/1brc-python/test_data:/onebrc/test_data \
-    --name onebrc-cpython_perf-modin onebrc-cpython_perf-modin \
-    make run-modin-ray-100t-docker | tee "$SCRIPT_DIR/../exec_logs/CPython_PerfOpt_Modin.log"
--- scripts/docker_run_all_cpython_perf_pyarrow_100m.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-
-docker run --privileged --rm -it --shm-size=8gb \
-    -v /home/oceord/Dev/1brc-python/test_data:/onebrc/test_data \
-    --name onebrc-cpython_perf onebrc-cpython_perf \
-    make run-pyarrow-100m-docker | tee "$SCRIPT_DIR/../exec_logs/CPython_PerfOpt_PyArrow.log"
